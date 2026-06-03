d4d3e34d077c75f6d398565513b749e23fd42897
JonVer403 <jcverhoeven@outlook.com>
2026-06-03 14:00:51 +0200

    penis

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -9,3 +9,16 @@ enum {
 var state = idlePistol
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+
+func _ready():
+	pass
+
+func _process(delta):
+	match state:
+		idlePistol:
+			animation_player.play("idlePistol")
+		runningPistol:
+			animation_player.play("runningPistol")
+		walkingPistol:
+			animation_player.play("walkingPistol")
