f06fd931cd3e1f8c4c355853619ff77c73bc2447
JonVer403 <jcverhoeven@outlook.com>
2026-06-14 22:15:26 +0200

    enemy draait

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -7,28 +7,32 @@ enum {
 
 var state = IDLE
 
+var target
+
+const TURN_SPEED = 2
+
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
+@onready var eyes: Node3D = $Eyes
 
 func _ready():
 	pass
 
-func _process(delta):
-	if ray_cast_3d.is_colliding():
+func _on_sightrange_body_entered(body: CharacterBody3D):
+	if body.is_in_group("Player"):
 		state = ALERT
-	else:
-		state = IDLE
+		target = body
+
+func _on_sightrange_body_exited(body: CharacterBody3D):
+	state = IDLE
+
+func _process(delta):
 	
 	match state:
 		IDLE:
 			animation_player.play("idlePistol")
 		ALERT:
 			animation_player.play("runningPistol")
-
-func _physics_process(delta):
-	var current_location = global_transform.origin
-	var next_location = nav_agent.get_next_path_position()
-
-func update_target_location(target_location):
-	nav_agent.target_position = target_loc  
+			eyes.look_at(target.global_transform.origin, Vector3.UP)
+			rotate_y(deg_to_rad(eyes.rotation.y * TURN_SPEED))
